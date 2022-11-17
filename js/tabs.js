26041a51fc48ade4d0cb55b5b9a9dabdaa470740
Alarma1 <zapara03@yandex.ru>
2022-11-17 21:08:08 +0500

    Добавил фаилы

---
+++ js/tabs.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        autoplay: {
+            delay: 5000,
+        },
+        loop: true,
+        speed: 800,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        // And if we need scrollbar
+        scrollbar: {
+            hide: true,
+        },
+        effect: 'slide',
+    })
+    document.querySelectorAll('.work__tabs').forEach(function (heroTabs) {
+        heroTabs.addEventListener('click', function (event) {
+            const path = event.currentTarget.dataset.path
+
+            document.querySelectorAll('.tab__content').forEach(function (tabContent) {
+                tabContent.classList.remove('tab__content-active')
+            })
+            document.querySelector(`[data-target="${path}"]`).classList.add('tab__content-active')
+
+            document.querySelectorAll('.work__tabs').forEach(function (workTabs) {
+                workTabs.classList.remove('tabs__color')
+            })
+            document.querySelector(`[data-path="${path}"]`).classList.add('tabs__color')
+        })
+    })
+    $(function () {
+        $("#accordion").accordion({
+            icons: { "header": "faq__icon-disabled", "activeHeader": "faq__icon-active" }
+        });
+    })
+    document.querySelectorAll('.head__burger').forEach(function (headBurger) {
+        headBurger.addEventListener('click', function (headBurger) {
+            document.querySelectorAll('.head__burger').forEach(function (headBurgerElement) {
+                headBurgerElement.classList.remove('head__burger-hide')
+            })
+            headBurger.currentTarget.classList.add('head__burger-hide')
+        })
+    })
+    // document.querySelectorAll('.search__btn').forEach(function (searchBtn) {
+    //     searchBtn.addEventListener('click', function (searchBtn) {
+    //         document.querySelectorAll('.head__field').forEach(function (headFieldElement) {
+    //             headFieldElement.classList.remove('head__field')
+    //         })
+    //         searchBtn.currentTarget.classList.add('head__field')
+    //     })
+    // })
+})
